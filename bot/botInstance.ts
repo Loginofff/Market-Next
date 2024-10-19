8e99f2211c9bccfa837211357feb096a2b056e6b
Dmitrijs Loginovs <132077317+Loginofff@users.noreply.github.com>
2024-10-19 11:33:39 +0200

    env

--- bot/botInstance.ts
+++ bot/botInstance.ts
@@ -1,4 +1,3 @@
 import { Telegraf } from 'telegraf';
 
-const TOKEN = "7671982064:AAEbormQ5xk2vZGh1gNJDIrsQDpev-gR0HY";
-export const bot = new Telegraf(TOKEN);
+export const bot = new Telegraf(process.env.BOT_TOKEN!);
